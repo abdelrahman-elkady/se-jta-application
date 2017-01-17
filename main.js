de9fc02eba148001a0561f48ff0b6229580b46b9
kady <elkadyabdelrahman@gmail.com>
2017-01-17 16:42:56 +0200

    Add confirmation endpoint

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ var _ = require('lodash');
 var jwt = require('jsonwebtoken');
 
 var config = require('./config');
+var ObjectID = require('mongodb').ObjectID;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -20,7 +21,12 @@ var applySchema = {
   lastName: Joi.string().min(3).max(600).required(),
   email: Joi.string().email().required(),
   mobile: Joi.string().length(11).required(),
-  personalWebsite: Joi.string()
+  personalWebsite: Joi.string().min(5)
+};
+
+var confirmSchema = {
+  references: Joi.array().items(Joi.object({title: Joi.string().required(), url: Joi.string().min(5)})).required(),
+  position: Joi.number().min(0).max(1)
 };
 
 database.connect(function(db, Applications) {
@@ -35,10 +41,10 @@ database.connect(function(db, Applications) {
         var message = '';
 
         for (var errorDetail of err.details) {
-          message += errorDetail.message + '\n';
+          message += errorDetail.message;
         }
 
-        return res.send(message);
+        return res.json({msg: message});
       } else {
         Applications.findOne({
           'email': req.body.email
@@ -58,16 +64,65 @@ database.connect(function(db, Applications) {
             };
 
             Applications.insertOne(application, function(err, doc) {
-              if(err) {
+              if (err) {
                 return res.json({msg: 'Something went wrong'});
               } else {
-                var token = jwt.sign({_id: doc.ops[0]._id}, config.secret, {expiresIn: '10 days'});
+                var token = jwt.sign({
+                  _id: doc.ops[0]._id
+                }, config.secret, {expiresIn: '10 days'});
                 return res.status(201).json({msg: 'Data submitted successfully !', token: token});
               }
             })
           }
         });
 
+      }
+    });
+
+  });
+
+  app.put('/api/applications/confirm', function(req, res) {
+    var token = req.get('Authorization');
+    if (_.isNil(token)) {
+      return res.json({msg: 'No token provided, please include your JWT in Authorization header'});
+    }
+
+    var application = jwt.verify(token, config.secret);
+    if (_.isNil(application)) {
+      return res.json({msg: 'Invalid token, please use a valid token'});
+    }
+    var applicationId = application._id;
+
+    Joi.validate(req.body, confirmSchema, function(err, value) {
+
+      if (err) {
+        var message = '';
+
+        for (var errorDetail of err.details) {
+          message += errorDetail.message;
+        }
+
+        return res.json({msg: message});
+      } else {
+        var references = req.body.references;
+        var position = req.body.position;
+
+        Applications.update({
+          _id: ObjectID(applicationId)
+        }, {
+          $set: {
+            position: position,
+            references: references
+          }
+        }, function(err, affectedDocs) {
+          if(affectedDocs.result.nModified === 0) {
+            return res.json({'msg': 'No data is changed, thank you anyways !'});
+          }
+
+          if (!err) {
+            return res.json({'msg': 'Great Job, your submission is successfully received, Thank you'});
+          }
+        });
       }
     });
 
